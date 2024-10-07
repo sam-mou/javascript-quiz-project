03532701f6366b9cd99849c75657625c8f953c8c
“Samy <samy.mouahbi@hotmail.com>
2024-10-07 15:27:18 +0200

    this is done

--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,38 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+    constructor (questions, timeLimit, timeRemaining) {
+        this.questions = questions;
+        this.timeLimit = timeLimit;
+        this.timeRemaining = timeRemaining;
+        this.correctAnswers = 0;
+        this.currentQuestionIndex = 0;
+    }
+getQuestion() { 
+    return this.questions[this.currentQuestionIndex];
+}   
+moveToNextQuestion() {
+    this.currentQuestionIndex++;
+}
 
-    // 2. getQuestion()
+shuffleQuestions() {
+    for (let i = this.questions.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.questions[i], this.questions[j]] = [this.questions[j], this.questions[i]];
+      }
+    }
+
+checkAnswer(answer) {
+    const currentQuestion = this.getQuestion();
     
-    // 3. moveToNextQuestion()
+    if (currentQuestion.answer === answer) {
+      this.correctAnswers++;
+      return true; 
+    } else {
+      return false; 
+    }
+  }
 
-    // 4. shuffleQuestions()
+hasEnded() {
+    return this.currentQuestionIndex >= this.questions.length;
+}
 
-    // 5. checkAnswer(answer)
-
-    // 6. hasEnded()
 }
